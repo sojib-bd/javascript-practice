f782040ffb1b2e0c255a9d919a81fb94c76ed6a4
sojib-bd <sojibrahmatuzzaman@gmail.com>
2020-02-25 22:58:57 +0600

    adding something methods

--- main.js
+++ main.js
@@ -35,3 +35,66 @@ console.log('My name is ' + name + ' and I am ' + age + ' years old')
 // Template string
 
 console.log(`My name is ${name} and I am ${age} years old`)
+
+
+//**************** Different kind of Methods */
+
+let str = "hello world!"
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.substring(0, 5).toUpperCase());
+
+let greeting = "hello world!"
+
+console.log(greeting.split(''))
+
+let multipleString = "template, technology, IT, company"
+
+console.log(multipleString.split(', '))
+
+//************************ Array ****************** */
+// is a variable that hold bunch of data type
+
+const fruits = ['apple', 'mango', 'banana'];
+
+//Adding something in the end of the array
+fruits.push('orange')
+console.log(fruits)
+
+//Adding something in the beginning 
+
+fruits.unshift('strawberry')
+console.log(fruits)
+
+// Delete the last one 
+fruits.pop()
+console.log(fruits)
+
+//Find the index of banana
+
+console.log(fruits.indexOf('banana'))
+
+
+// object 
+
+const person = {
+    firstName: "jon",
+    lastName: 'Doe',
+    age: 30,
+    hobbies: ['movies', 'music', 'game'],
+    address: {
+        city: 'boston'
+    }
+}
+
+// Destructure
+const { firstName, lastName } = person
+console.log(firstName, lastName)
+
+// arguments or operator
+
+let a = 1,
+    b = 3;
+
+alert(b - a)
